16be09d83e8c4705bdf3a2a905f743a82c66d439
Eren Kara <eren.kara@trendyol.com>
2022-08-26 14:48:45 +0300

    SM-1117 missing brackets and null collision added

--- src/crawler/meta-crawler.ts
+++ src/crawler/meta-crawler.ts
@@ -18,10 +18,10 @@ export class MetaCrawler implements Crawler {
     public collect(): META_DATA {
 
         const description = (<HTMLMetaElement>(document.querySelector('meta[name="description"]')))?.content || "";
-        const ogTitle = (<HTMLMetaElement>(document.querySelector('meta[property="og:title"')))?.content || "";
-        const ogDescription = (<HTMLMetaElement>(document.querySelector('meta[property="og:description"'))).content || "";
+        const ogTitle = (<HTMLMetaElement>(document.querySelector('meta[property="og:title"]')))?.content || "";
+        const ogDescription = (<HTMLMetaElement>(document.querySelector('meta[property="og:description"]')))?.content || "";
         const ogImage = (<HTMLMetaElement>(document.querySelector('meta[property="og:image"]')))?.content || "";
-        const canonical = (<HTMLLinkElement>(document.querySelector('link[rel="canonical"')))?.href  || "";
+        const canonical = (<HTMLLinkElement>(document.querySelector('link[rel="canonical"]')))?.href  || "";
         const h1Tag = (<HTMLHeadingElement>(document.querySelector('h1')))?.textContent || "";
         const title = document.title;
         
