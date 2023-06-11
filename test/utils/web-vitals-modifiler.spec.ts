feb91e06355db374cf342f01fb81af91855da592
mert.tosun <mert.tosun@windowslive.com>
2023-06-11 20:22:46 +0300

    DR-515 add og wrapper tests

--- test/utils/web-vitals-modifiler.spec.ts
+++ test/utils/web-vitals-modifiler.spec.ts
@@ -24,7 +24,7 @@ describe('crawler-factory works as a charm', () => {
     expect(res[CLS_SHORT_NAME].value).toEqual(0)
   })
 
-  test('prepareCoreWebVitalsMetricsFromEntries should return correct status for values', () => {
+  test('prepareCoreWebVitalsMetricsFromEntries should return correct status for values (first variation)', () => {
     const res = prepareCoreWebVitalsMetricsFromEntries([
       {
         entryType: 'largest-contentful-paint',
@@ -76,5 +76,102 @@ describe('crawler-factory works as a charm', () => {
     expect(res[LCP_SHORT_NAME].value).toEqual(1)
     expect(res[FID_SHORT_NAME].value).toEqual(140)
     expect(res[CLS_SHORT_NAME].value).toEqual(0.31)
+  })
+
+  test('prepareCoreWebVitalsMetricsFromEntries should return correct status for values (second variation)', () => {
+    const res = prepareCoreWebVitalsMetricsFromEntries([
+      {
+        entryType: 'largest-contentful-paint',
+        startTime: 2700,
+        duration: 500,
+        name: '',
+        toJSON() {
+          return {}
+        },
+      },
+      {
+        entryType: 'first-input',
+        startTime: 10,
+        duration: 30,
+        processingStart: 109,
+        name: '',
+        toJSON() {
+          return {}
+        },
+      },
+      {
+        entryType: 'layout-shift',
+        startTime: 0,
+        duration: 0,
+        processingStart: 0,
+        name: '',
+        value: 0.05,
+        toJSON() {
+          return {}
+        },
+      },
+      {
+        entryType: 'layout-shift',
+        startTime: 0,
+        duration: 0,
+        processingStart: 0,
+        name: '',
+        value: 0.06,
+        toJSON() {
+          return {}
+        },
+      },
+    ])
+
+    expect(res[LCP_SHORT_NAME].status).toEqual('needImprovement')
+    expect(res[FID_SHORT_NAME].status).toEqual('good')
+    expect(res[CLS_SHORT_NAME].status).toEqual('needImprovement')
+
+    expect(res[LCP_SHORT_NAME].value).toEqual(2.7)
+    expect(res[FID_SHORT_NAME].value).toEqual(99)
+    expect(res[CLS_SHORT_NAME].value).toEqual(0.11)
+  })
+
+  test('prepareCoreWebVitalsMetricsFromEntries should return correct status for values (third variation)', () => {
+    const res = prepareCoreWebVitalsMetricsFromEntries([
+      {
+        entryType: 'largest-contentful-paint',
+        startTime: 5500,
+        duration: 500,
+        name: '',
+        toJSON() {
+          return {}
+        },
+      },
+      {
+        entryType: 'first-input',
+        startTime: 5,
+        duration: 30,
+        processingStart: 335,
+        name: '',
+        toJSON() {
+          return {}
+        },
+      },
+      {
+        entryType: 'layout-shift',
+        startTime: 0,
+        duration: 0,
+        processingStart: 0,
+        name: '',
+        value: 0.01,
+        toJSON() {
+          return {}
+        },
+      },
+    ])
+
+    expect(res[LCP_SHORT_NAME].status).toEqual('bad')
+    expect(res[FID_SHORT_NAME].status).toEqual('bad')
+    expect(res[CLS_SHORT_NAME].status).toEqual('good')
+
+    expect(res[LCP_SHORT_NAME].value).toEqual(5.5)
+    expect(res[FID_SHORT_NAME].value).toEqual(330)
+    expect(res[CLS_SHORT_NAME].value).toEqual(0.01)
   })
 })
